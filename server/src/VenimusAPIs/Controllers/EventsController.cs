5f4308013450320a16c5bf5904eb518d6675e40f
David Betteridge <david.betteridge@proactis.com>
2019-10-19 22:35:20 +0100

    Add API to create events

---
+++ server/src/VenimusAPIs/Controllers/EventsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VenimusAPIs.ViewModels;
+
+namespace VenimusAPIs.Controllers
+{
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly Services.Mongo _mongo;
+
+        private readonly IMapper _mapper;
+
+        public EventsController(Services.Mongo mongo, IMapper mapper)
+        {
+            _mongo = mongo;
+            _mapper = mapper;
+        }
+
+        [Route("api/groups/{groupName}/events")]
+        [Authorize]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Post([FromRoute] string groupName, [FromBody] CreateNewEvent newEvent)
+        {
+            var model = _mapper.Map<Models.Event>(newEvent);
+            model.EventID = Guid.NewGuid().ToString();
+
+            await _mongo.StoreEvent(model);
+            return CreatedAtRoute("Events", new { groupName = groupName,  eventID = model.EventID }, newEvent);
+        }
+
+        [Authorize]
+        [Route("api/groups/{groupName}/events/{eventID}", Name = "Events")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<GetEvent> Get([FromRoute] string groupName, [FromRoute] string eventID)
+        {
+            return NotFound();
+        }
+    }
+}
---
+++ server/src/VenimusAPIs/Models/Event.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using MongoDB.Bson;
+
+namespace VenimusAPIs.Models
+{
+    public class Event
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:Element should begin with upper-case letter", Justification = "Required for Mongo")]
+        public ObjectId _id { get; set; }
+
+        /// <summary>
+        /// Unique ID for the event
+        /// </summary>
+        public string EventID { get; set; }
+
+        /// <summary>
+        /// The title of the event,  for example March 2019 Meetup. Must be unique for the group.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// A description of the event,  in markdown format.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The location of the event, for example York Minster,  Room 201 York St John University
+        /// </summary>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// Date and time the event starts
+        /// </summary>
+        public DateTime StartTime { get; set; }
+
+        /// <summary>
+        /// Date and time the event ends.  Must be after the StartTime
+        /// </summary>
+        public DateTime EndTime { get; set; }
+    }
+}
