fce218350761771b575d442619cb7ca4ac91dbda
Chris Maughan <cmaughan@snarlsoftware.com>
2019-10-30 11:21:09 +0000

    Added Vue.js sample site that connects to Auth0
    This is just the boilerplate Vue.js sample to connect to Auth0; assuming
    that Vue.js is going to be one of our front ends.
    
    I believe that Auth0 needs to setup callback URLs in the App Settings.
    The following dev settings:
    
    Callback URL: http://localhost:3000/callback
    Allowed Origins: http://localhost:3000
    Allowed Logout: http://localhost:3000
    
    Then I _think_ it should work.
    
    Boot this app with:
    
    npm install
    npm run serve
    
    Then you can navigate to http://localhost:3000
    
    The Login button is not visible yet, but it should be when the above
    settings are changed in Auth0.

---
+++ frontend/src/components/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center hero">
+    <img class="mb-3 app-logo" src="/logo.png" alt="Vue.js logo" width="120" />
+    <h1 class="mb-4">Venimus</h1>
+    <p class="lead">
+    This is the Venimus front end website
+      <a
+        href="https://vuejs.org"
+      >Vue.js</a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero"
+};
+</script>
