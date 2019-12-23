735256f0e59e55232c65ac01154120ab3970883b
David Betteridge <DavidaBetteridge@gmail.com>
2019-12-23 10:28:30 +0000

    Add view event details page

---
+++ yorkdevelopersevents/Pages/ViewEventDetails.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+using YorkDeveloperEvents.Services;
+using YorkDeveloperEvents.ViewModels;
+
+namespace YorkDeveloperEvents
+{
+    public class ViewEventDetailsModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public string GroupSlug { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string EventSlug { get; set; }
+
+        [BindProperty]
+        public GetEvent ViewModel { get; set; }
+
+        public async Task OnGet([FromServices] API api)
+        {
+            ViewModel = await api.GetEvent(GroupSlug, EventSlug);
+        }
+    }
+}
